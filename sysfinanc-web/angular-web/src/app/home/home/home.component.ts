d03b8f0b2ead8eeb9eca52ebb564bed82e39e313
Wallace Rodrigo <wallacerodrigon@gmail.com>
2018-07-15 23:09:04 -0300

    ajustes de telas (header, home e menu)

--- sysfinanc-web/angular-web/src/app/home/home/home.component.ts
+++ sysfinanc-web/angular-web/src/app/home/home/home.component.ts
@@ -11,11 +11,34 @@ import {UsuarioVO} from '../../dominio/vo/usuario-vo';
 })
 export class HomePageComponent {
 
+    private classeHidden: string= 'hidden';
+
     constructor( public _dialogService: DialogService, public router: Router){
     }
     
     logout(){
         sessionStorage.clear();
         this.router.navigate(['']);
+    }
+
+    toggleSubMenu(idSubMenu: string, idMenuIcone?: string){
+        var elemento = document.getElementById(idSubMenu);
+        var classes = elemento.className.split(' ');
+        var getIndex = classes.indexOf(this.classeHidden);
+        
+        if (getIndex === -1) {
+            classes.push(this.classeHidden);
+        } else {
+            classes.splice(getIndex, this.classeHidden.length);
+        }
+        elemento.className = classes.join(' ');
+
+        //está visível, trocar para (-). Se está invisível, trocar para (+)
+        if (idMenuIcone && getIndex === -1){
+            document.getElementById(idMenuIcone).className = 'glyphicon glyphicon-plus-sign';
+        } else {
+            document.getElementById(idMenuIcone).className = 'glyphicon glyphicon-minus-sign';
+        }
+
     }
 }
