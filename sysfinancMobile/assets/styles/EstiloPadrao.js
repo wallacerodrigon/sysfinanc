b2e6db5edf8416f05437a9422a84aa60cc261018
Wallace Rodrigo <wallacerodrigon@gmail.com>
2018-10-27 15:29:33 -0300

    Tela de login versão inicial

---
+++ sysfinancMobile/assets/styles/EstiloPadrao.js
@@ -0,0 +1,16 @@
+import {StyleSheet} from 'react-native';
+
+const EstiloPadrao = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    sombra: {
+        shadowColor: '#666',
+        shadowOpacity: 0.3,
+        shadowRadius:30
+    }
+});
+
+export default EstiloPadrao;
