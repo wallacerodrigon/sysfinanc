31a7eedb273c5eb381fb88dcf78356c9f6ecc7b0
Wallace Sousa <wallacerodrigon@gmail.com>
2018-12-23 15:57:57 -0200

    sh de deploy

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
-HOST="10.199.199.2"
-USER="user01"
-PASSWD="pass01"
-FILE="filebkp.rar"
-DIR="/backup"
-
-cd ${DIR}
+HOST="walltec.net.br"
+USER="wallace"
+PASSWD="Forro008"
+FILEAPI="./sysfinanc-api/target/sysfinanc-api.war"
+FILEWEB="./sysfinanc-web/target/sysfinanc-web.war"
+DIR="/appservers/wildfly-9.0.1.Final/standalone/deployments"
 
 ftp -n ${HOST} <<EOF
 quote user ${USER}
 quote PASS ${PASSWD}
-put ${FILE}
+cd ${DIR}
+put ${FILEAPI}
+put ${FILEWEB}
 quit
 EOF
