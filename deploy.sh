02aafa670b8ef43c52cb4d2b938cfbfa9dadc70f
Wallace Sousa <wallacerodrigon@gmail.com>
2018-12-29 21:09:02 -0200

    Ajustes para geração da versão

--- deploy.sh
+++ deploy.sh
@@ -3,15 +3,30 @@
 HOST="walltec.net.br"
 USER="wallace"
 PASSWD="Forro008"
-FILEAPI="./sysfinanc-api/target/sysfinanc-api.war"
-FILEWEB="./sysfinanc-web/target/sysfinanc-web.war"
+FILEAPI="./sysfinanc-api.war"
+FILEWEB="./sysfinanc-web.war"
 DIR="/appservers/wildfly-9.0.1.Final/standalone/deployments"
+
+cd ./sysfinanc-api/target
 
 ftp -n ${HOST} <<EOF
 quote user ${USER}
 quote PASS ${PASSWD}
 cd ${DIR}
+del sysfinanc-api.war.deployed
+ren sysfinanc-api.war sysfinanc-api.war.bkp
 put ${FILEAPI}
+EOF
+
+cd ../../sysfinanc-web/target
+
+ftp -n ${HOST} <<EOF
+quote user ${USER}
+quote PASS ${PASSWD}
+cd ${DIR}
+del sysfinanc-api.war.deployed
+ren sysfinanc-web.war sysfinanc-web.war.bkp
 put ${FILEWEB}
 quit
 EOF
+
