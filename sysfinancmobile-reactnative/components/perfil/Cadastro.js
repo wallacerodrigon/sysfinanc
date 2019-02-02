9653a8578a3ab0e9db1d2a6046abff3bb922d620
Wallace Sousa <wallacerodrigon@gmail.com>
2019-02-02 17:18:03 -0200

    Teste com mapa mostrando a minha casa

--- sysfinancmobile-reactnative/components/perfil/Cadastro.js
+++ sysfinancmobile-reactnative/components/perfil/Cadastro.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 import EstilosComuns from '../../assets/estilos/estilos';
+import { MapView } from 'expo';
 
 export default class Cadastro extends React.Component {
     static navigationOptions = {
@@ -9,9 +10,19 @@ export default class Cadastro extends React.Component {
       };
 
     render() {
+        
         return (
             <View style={EstilosComuns.container}>
                 <Text>Cadastro</Text>
+                <MapView
+                    style={{ flex: 1 }}
+                    initialRegion={{
+                    latitude: -15.835025,
+                    longitude: -48.089085,
+                    latitudeDelta: 0.422,
+                    longitudeDelta: 0.221,
+                    }}
+                />                
             </View>
         )
     };
